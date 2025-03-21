fa2149cfbb60265c4a320cf453330525afcc3a15
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2025-03-21 17:48:14 +0900

    rm all casts but cantelli

--- theories/probability.v
+++ theories/probability.v
@@ -519,8 +519,10 @@ Proof.
 move=> X2.
 have Pfin : P setT \is a fin_num := fin_num_measure P _ measurableT.
 have X1 := lfun_inclusion12 Pfin X2.
-rewrite /variance covarianceZl ?covarianceZr ?lfun2M2_1 ?lfunp_scale ?ler1n//.
-by rewrite muleA EFinM.
+rewrite /variance covarianceZl//=.
+- by rewrite covarianceZr// ?muleA ?EFinM// lfun2M2_1.
+- by rewrite lfunp_scale.
+- by rewrite lfun2M2_1// lfunp_scale// ler1n.
 Qed.
 
 Lemma varianceN (X : T -> R) : X \in lfun P 2%:E -> 'V_P[(\- X)%R] = 'V_P[X].
